b9a34c74d70399d19491e822d343cf59b09cb57c
xMasTeR <43861336+iiMostafaYT@users.noreply.github.com>
2019-01-06 00:55:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,21 @@ message.channel.sendEmbed(embed)
 }); 
 
 
- 
+ client.on('message', message => {
+    if (message.content === ('.bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
  
  client.login(process.env.BOT_TOKEN);
